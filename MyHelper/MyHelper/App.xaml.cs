60bae8d5b516c34f794d48bc1f3fb99b37b82609
xiaoyuzhzh <xiaoyuzhzh@163.com>
2018-05-10 21:34:51 +0800

    工具换新的app

---
+++ MyHelper/MyHelper/App.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// This code is almost identical to the Blank Application code.
+// The key difference is that this project includes the declaration of 
+// SupportsMultipleInstances in the package.appxmanifest.
+
+namespace MyHelper
+{
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                   //TODO: Load state from previously suspended application.
+                }
+                Window.Current.Content = rootFrame;
+            }
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                Window.Current.Activate();
+            }
+
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+
+            var viewTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+            if (viewTitleBar != null)
+            {
+                viewTitleBar.ButtonBackgroundColor = Colors.Transparent;
+                viewTitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+                viewTitleBar.ButtonForegroundColor = (Color)Resources["SystemBaseHighColor"];
+            }
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            SuspendingDeferral deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity.
+            deferral.Complete();
+        }
+    }
+}
---
+++ MyHelper/MyHelper/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Tool.Pages;
+using Windows.ApplicationModel.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MyHelper
+{
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //SystemNavigationManager.GetForCurrentView().BackRequested += SystemNavigationManager_BackRequested;
+
+            //SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+
+            //this.frame.Navigated += OnNavigatedToPage;
+
+            //使用没有宽度的block，替换原来的TitleBar，这样可以保证扩展到顶部的按钮不会被阻挡
+            //Window.Current.SetTitleBar(AppTitle);
+
+
+            //var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            //Window.Current.CoreWindow.SizeChanged += (s, e) => UpdateAppTitle();
+            //coreTitleBar.LayoutMetricsChanged += (s, e) => UpdateAppTitle();
+        }
+
+        void UpdateAppTitle()
+        {
+            var full = (ApplicationView.GetForCurrentView().IsFullScreenMode);
+            var left = 12 + (full ? 0 : CoreApplication.GetCurrentView().TitleBar.SystemOverlayLeftInset);
+            AppTitle.Margin = new Thickness(left, 8, 0, 0);
+        }
+
+
+        private void NavigationView_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+
+            }
+            else
+            {
+                var menu = args.SelectedItem as NavigationViewItem;
+                switch (menu.Tag.ToString())
+                {
+                    case "codeChange":
+                        contentFrame.Navigate(typeof(CodeConvert));
+                        ContentHeader.Text = "编码转换";
+                        break;
+                    case "timestampTranslate":
+                        contentFrame.Navigate(typeof(TimestampConvert));
+                        ContentHeader.Text = "时间戳转换";
+                        break;
+                    case "jsonFormat":
+                        contentFrame.Navigate(typeof(JSONFormat));
+                        ContentHeader.Text = "json转换";
+                        break;
+                }
+            }
+        }
+
+        private void NavView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+
+        }
+    }
+}
